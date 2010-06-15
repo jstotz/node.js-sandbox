b63ab1060cbd270faa2612c752b75e2c5b9feed7
Jay Stotz <jason.stotz@gmail.com>
2010-06-14 23:35:15 -0400

    Added a few more sample route definitions

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
 var sys = require('sys'),
     http = require('http');
     router = require('./lib/router').createRouter();
-    repl = require('repl');
 
 router.get('/', function() {
   this.response.writeHead(200, {'Content-Type': 'text/plain'});
   this.response.end('Hello, world!');
 });
 
-router.get('/hello/:name', function(name) {
-  this.response.writeHead(200, {'Content-Type': 'text/plain'});
-  this.response.end('Hello, ' + name + '!');
-});
-
 router.get('/dog', function() {
   this.response.writeHead(200, {'Content-Type': 'text/plain'});
   this.response.end('Sup, dog!');
+});
+
+router.get('/hello/:name', function(name) {
+  this.response.writeHead(200, {'Content-Type': 'text/plain'});
+  this.response.end('Hello, ' + name + '. Good ' + this.params.time_of_day + '!');
+});
+
+router.get('/hello/:name/:time_of_day', function(name) {
+  this.response.writeHead(200, {'Content-Type': 'text/plain'});
+  this.response.end('Hello, ' + name + '. Good ' + this.params.time_of_day + '!');
+});
+
+router.get('/users/:username/:admin', function(username, admin) {
+  this.response.writeHead(200, {'Content-Type': 'application/json'});
+  this.response.end('{"username": "' + username + '", "admin": ' + admin + '}');
+});
+
+router.get('/directory/*', function(hierarchy) {
+  this.response.writeHead(200, {'Content-Type': 'text/plain'});
+  this.response.end(hierarchy.split('/').join(', '));
 });
 
 var httpServer = http.createServer(function (req, res) {
